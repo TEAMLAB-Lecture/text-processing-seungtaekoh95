4b3a3580ff25183ba93f894af85fc2cf588e9faa
seungtaekoh95 <seungtaekoh95@gmail.com>
2021-01-20 17:55:16 +0900

    Update my code

--- text_processing.py
+++ text_processing.py
@@ -34,9 +34,9 @@ def normalize(input_string):
              'extra space'
     """
     normalized_string = None
+    input_string.lower() # 문자열을 소문자로 변환
     input_string.strip() # 문자열의 양 옆 공백 제거
     input_string.replace("  ", " ") # 문자열의 두 칸 이상의 공백을 한 칸으로 변환
-    input_string.lower() # 문자열을 소문자로 변환
     normalized_string = input_string
     return normalized_string
 
